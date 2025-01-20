f5423d21f954a2c99c6b65de308359e621fca1c4
Ekow Daniels <ftn2mr@virginia.edu>
2025-01-20 15:19:34 -0500

    modified build script

--- build_project.sh
+++ build_project.sh
@@ -1,3 +1,19 @@
-rm -rf build/ devel/
+#!/bin/bash
+
+PROJECT_PATH="$HOME/Humanoid-Robot-ROS"
+
+if [ -d "$PROJECT_PATH/build" ]; then
+    echo "Removing existing build directory..."
+    rm -rf "$PROJECT_PATH/build"
+fi
+
+if [ -d "$PROJECT_PATH/devel" ]; then
+    echo "Removing existing devel directory..."
+    rm -rf "$PROJECT_PATH/devel"
+fi
+
 catkin_make 
-source devel/setup.bash
+
+echo "Sourcing the workspace..."
+echo $PROJECT_PAT
+source "$PROJECT_PATH/devel/setup.bash"

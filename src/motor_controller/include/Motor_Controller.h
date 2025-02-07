75f98d64a39f3a818e6403686c16a4f869e99320
Ekow Daniels <ftn2mr@virginia.edu>
2025-02-07 17:22:58 -0500

    fixed reverse issue

--- src/motor_controller/include/Motor_Controller.h
+++ src/motor_controller/include/Motor_Controller.h
@@ -6,7 +6,7 @@
 #include <string>
 #include <sensor_msgs/JointState.h>
 #include <boost/algorithm/clamp.hpp>
-#include <unistd.h> 
+#include <unistd.h>
 
 // TODO: add hard coding, for knee and motor joints (set motor x to this positon and so on for the test)
 class Motor_Controller
@@ -20,25 +20,13 @@ public:
     int get_goal_position();
     int get_starting_position();
     int get_operating_mode();
-    int set_starting_position(int position);
 
     bool get_reverse();
-    bool connect_motor();
-
-    void set_goal_position(int position);
-    void set_torque(bool torque);
-    void set_operating_mode(int mode);
-
-    void write_torque();
-    void write_goal_position();
-    void write_operating_mode();
-    void publish_motor_data();
 
     void set_max_motor_degrees(int max_motor_degrees);
-    void reset_motor();
-
-    void set_offset();
-    void go_to_starting_position();
+    void set_goal_position(int position);
+    void write_goal_position();
+    void publish_motor_data();
 
 private:
     int motor_id;
@@ -48,6 +36,7 @@ private:
     int baude_rate;
     int max_motor_position;
     int starting_position;
+    int drive_mode;
     float protocol_version;
     bool torque;
     bool motor_connected;
@@ -55,5 +44,15 @@ private:
 
     dynamixel::PortHandler *port_handler;
     dynamixel::PacketHandler *packet_handler;
+
+    int set_starting_position(int position);
+
+    bool connect_motor();
+    void set_torque(bool torque);
+    void set_operating_mode(int mode);
+    void write_torque();
+    void write_operating_mode();
+    void reset_motor();
+    void set_drive_mode(int drive_mode);
 };
 #endif // MOTOR_CONTROLLER_H
